697d3717443f27636787c3300f782a754c9104ba
Maurk <mauricio.lp@astrumla.com>
2021-12-02 20:52:44 -0500

    fix: security credentials hidden

--- Backend/config/config.py
+++ Backend/config/config.py
@@ -1,15 +1,17 @@
 '''
 config.py: modulo donde se configura la aplicación
 '''
-from flask import Flask
+import os
+from dotenv import load_dotenv
 from flask_cors import CORS
 from mongoengine import connect
 
 def config_app(app):
-    app.secret_key = 'clavesecreta'
 
-    DB_URI = "mongodb+srv://Mauricio:1234@fisibici"
-    DB_URI += ".cpmx7.mongodb.net/SistemaBicicletas?retryWrites=true&w=majority"
+    load_dotenv()
+    app.secret_key = os.getenv('CLAVE_SECRETA', '')
+
+    DB_URI = os.getenv('DB_URI', '')
     connect(host=DB_URI)
 
     CORS(app=app, supports_credentials=True)
@@ -26,8 +28,8 @@ def config_app_production(app):
 def config_app_development(app):
     config_app(app)
 
-    #app.config.update(
-    #    SERVER_NAME='127.0.0.1:5000',
-    #    SESSION_COOKIE_NAME='127.0.0.1:5000',
-    #    SESSION_COOKIE_DOMAIN='127.0.0.1:5000',
-    #)
+    app.config.update(
+        SERVER_NAME='127.0.0.1:5000',
+        SESSION_COOKIE_NAME='127.0.0.1:5000',
+        SESSION_COOKIE_DOMAIN='127.0.0.1:5000',
+    )
